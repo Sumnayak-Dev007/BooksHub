c9f0c8c1b10a73f04d1f4e5c837c23ed7c2063e9
Sumnayak-Dev007 <nsumankumari225@gmail.com>
2025-10-15 19:34:34 +0530

    Search Box

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,13 @@
 import { HStack, Image, Text } from '@chakra-ui/react'
 import logo from "../assets/brain.svg"
 import ColormodeSwitch from './ColormodeSwitch'
+import SearchInput from './SearchInput'
 
 function Navbar() {
   return (
     <HStack justifyContent={'space-between'} padding={'15px'}>
-        <Image src={logo}  boxSize="55px"/>
+        <Image src={logo}  boxSize="60px"/>
+        <SearchInput/>
         <Text></Text>
         <ColormodeSwitch/>
     </HStack>
